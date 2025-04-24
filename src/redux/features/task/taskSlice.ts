cdfb86b2e68d2d9b822871332661e558f987f192
solaiman-siam <solaimansiam93@gmail.com>
2025-04-24 20:10:36 +0600

    some changes

--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -1,6 +1,6 @@
 import { RootState } from "@/redux/store";
 import { ITask } from "@/types";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface IInitialState {
     tasks: ITask[],
@@ -30,6 +30,18 @@ const initialState : IInitialState = {
 }
 
 
+
+
+const taskSlice = createSlice({
+    name: 'task',
+    initialState,
+    reducers: {
+        addTask: (state, action : PayloadAction<ITask>) => {
+
+            state.tasks.push(action.payload)
+        }
+    }
+})
 export const selectTask = ((state : RootState) =>{
     return state.todo.tasks
 })
@@ -38,12 +50,6 @@ export const selectFilter = ((state : RootState) => {
     return state.todo.filter
 })
 
-const taskSlice = createSlice({
-    name: 'task',
-    initialState,
-    reducers: {}
-})
-
-
+export const {addTask} = taskSlice.actions
 
 export default taskSlice.reducer
