37bc3d63e9ec5128a2c438736d081b321e9ef66e
solaiman-siam <solaimansiam93@gmail.com>
2025-05-07 09:09:56 +0600

    added rtq query base api setup

---
+++ src/redux/features/user/userSlice.ts
@@ -0,0 +1,13 @@
+
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+
+export const baseApi = createApi({
+    reducerPath: 'baseApi',
+    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:5000/api'}),
+    endpoints: (builder) => ({
+        getTask: builder.query({
+            query: () => '/tasks'
+        })
+    })
+})
